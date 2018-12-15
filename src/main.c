cf12ae2a9b7b62ed36508475acc5b3db589373a7
AlejandroColomar <colomar.6.4.3@gmail.com>
2018-12-15 15:09:37 +0100

    Put sysclk_config() into a separate module "clk"

--- src/main.c
+++ src/main.c
@@ -1,49 +1,58 @@
-/**
-  ******************************************************************************
-  * This is the template for Informatica Industrial II. Lunes tarde group.
-  * Based on the STMicroelectronics HAL template
-  *
-  * See http://aperles.blogs.upv.es/arm-cortex-m-practico-1-introduccion-a-los-microcontroladores-stm32-de-st/
-  *
-  * Good look,
-  * Angel Perles
-  ******************************************************************************
-  */
-
-/* Includes ------------------------------------------------------------------*/
-#include <stdbool.h>
-#include <stdio.h>
-
-#include "stm32l4xx_hal.h"
-
-#include "delay.h"
-#include "led.h"
-#include "servo.h"
-
-#include "led_test.h"
-#include "servo_test.h"
-
-/* Private typedef -----------------------------------------------------------*/
-/* Private define ------------------------------------------------------------*/
-/* Private macro -------------------------------------------------------------*/
-/* Private variables ---------------------------------------------------------*/
-/* Private function prototypes -----------------------------------------------*/
-	static	void	sys_clock_config	(void);
-//	static	void	Error_Handler		(void);
-//		void	fputc_SetXY		(uint16_t x, uint16_t y);
+/******************************************************************************
+ *	main.c								      *
+ *	2018/dec/15							      *
+ ******************************************************************************/
 
 
-/* Private functions ---------------------------------------------------------*/
+/******************************************************************************
+ ******* headers **************************************************************
+ ******************************************************************************/
+/* Standard C ----------------------------------------------------------------*/
+	#include <stdbool.h>
+	#include <stdio.h>
 
-/** Main program **************************************************************/
+/* Drivers -------------------------------------------------------------------*/
+	#include "stm32l4xx_hal.h"
+
+/* libalx --------------------------------------------------------------------*/
+/* STM32L4 modules -----------------------------------------------------------*/
+	#include "clk.h"
+	#include "delay.h"
+	#include "led.h"
+	#include "servo.h"
+
+	#include "led_test.h"
+	#include "servo_test.h"
+
+
+/******************************************************************************
+ ******* macros ***************************************************************
+ ******************************************************************************/
+
+
+/******************************************************************************
+ ******* variables ************************************************************
+ ******************************************************************************/
+/* Global --------------------------------------------------------------------*/
+/* Static --------------------------------------------------------------------*/
+
+
+/******************************************************************************
+ ******* static functions (prototypes) ****************************************
+ ******************************************************************************/
+//	void	fputc_SetXY		(uint16_t x, uint16_t y);
+
+
+/******************************************************************************
+ ******* main *****************************************************************
+ ******************************************************************************/
 int	main	(void)
 {
-
-	/* STM32F4xx HAL library initialization */
+	/* STM32L4xx HAL library initialization */
 	HAL_Init();
 
 	/* Configure the System clock to 180 MHz */
-	sys_clock_config();
+	sysclk_config();
 
 	/* init */
 	led_init();
@@ -66,62 +75,11 @@ int	main	(void)
 }
 
 
-/**
-  * @brief  System Clock Configuration
-  *         The system Clock is configured as follow : 
-  *            System Clock source            = PLL (MSI)
-  *            SYSCLK(Hz)                     = 80000000
-  *            HCLK(Hz)                       = 80000000
-  *            AHB Prescaler                  = 1
-  *            APB1 Prescaler                 = 1
-  *            APB2 Prescaler                 = 1
-  *            MSI Frequency(Hz)              = 4000000
-  *            PLL_M                          = 1
-  *            PLL_N                          = 40
-  *            PLL_R                          = 2
-  *            PLL_P                          = 7
-  *            PLL_Q                          = 4
-  *            Flash Latency(WS)              = 4
-  * @param  None
-  * @retval None
-  */
-static	void	sys_clock_config	(void)
-{
-	RCC_ClkInitTypeDef RCC_ClkInitStruct;
-	RCC_OscInitTypeDef RCC_OscInitStruct;
+/******************************************************************************
+ ******* static functions (definitions) ***************************************
+ ******************************************************************************/
 
-	/* MSI is enabled after System reset, activate PLL with MSI as source */
-	RCC_OscInitStruct.OscillatorType = RCC_OSCILLATORTYPE_MSI;
-	RCC_OscInitStruct.MSIState = RCC_MSI_ON;
-	RCC_OscInitStruct.MSIClockRange = RCC_MSIRANGE_6;
-	RCC_OscInitStruct.MSICalibrationValue = RCC_MSICALIBRATION_DEFAULT;
-	RCC_OscInitStruct.PLL.PLLState = RCC_PLL_ON;
-	RCC_OscInitStruct.PLL.PLLSource = RCC_PLLSOURCE_MSI;
-	RCC_OscInitStruct.PLL.PLLM = 1;
-	RCC_OscInitStruct.PLL.PLLN = 40;
-	RCC_OscInitStruct.PLL.PLLR = 2;
-	RCC_OscInitStruct.PLL.PLLP = 7;
-	RCC_OscInitStruct.PLL.PLLQ = 4;
-	if(HAL_RCC_OscConfig(&RCC_OscInitStruct) != HAL_OK) {
-		/* Initialization Error */
-		while(true) {
-			;
-		}
-	}
 
-	/* Select PLL as system clock source and configure the HCLK, PCLK1
-	 * and PCLK2 clocks dividers */
-	RCC_ClkInitStruct.ClockType = (RCC_CLOCKTYPE_SYSCLK | RCC_CLOCKTYPE_HCLK | RCC_CLOCKTYPE_PCLK1 | RCC_CLOCKTYPE_PCLK2);
-	RCC_ClkInitStruct.SYSCLKSource = RCC_SYSCLKSOURCE_PLLCLK;
-	RCC_ClkInitStruct.AHBCLKDivider = RCC_SYSCLK_DIV1;
-	RCC_ClkInitStruct.APB1CLKDivider = RCC_HCLK_DIV1;  
-	RCC_ClkInitStruct.APB2CLKDivider = RCC_HCLK_DIV1;  
-	if(HAL_RCC_ClockConfig(&RCC_ClkInitStruct, FLASH_LATENCY_4) != HAL_OK) {
-		/* Initialization Error */
-		while(true) {
-			;
-		}
-	}
-}
-
-/************************ (C) COPYRIGHT STMicroelectronics *****END OF FILE****/
+/******************************************************************************
+ ******* end of file **********************************************************
+ ******************************************************************************/
