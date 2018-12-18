c9cfbaf2425cd4d6b61d022076988e293c692176
AlejandroColomar <colomar.6.4.3@gmail.com>
2018-12-18 01:49:53 +0100

    Include stdbool.h

--- src/stm32l4xx_it.c
+++ src/stm32l4xx_it.c
@@ -38,7 +38,7 @@
   */
 
 /* Includes ------------------------------------------------------------------*/
-#include <stdint.h>
+#include <stdbool.h>
 #include "stm32l4xx_hal.h"
 #include "stm32l4xx_it.h"
 
